7920ca3bf8e039038a4f995c71e900bca48143e4
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-02-29 23:53:29 +0300

    lesson-5 part-2

--- src/Core/Security/Entities/User.cs
+++ src/Core/Security/Entities/User.cs
@@ -4,7 +4,7 @@ namespace Core.Security.Entities;
 
 public class User : Entity
 {
-    public string FirstName{ get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public byte[] PasswordSalt { get; set; }
