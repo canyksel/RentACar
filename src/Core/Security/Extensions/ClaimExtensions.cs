fced0c0af67707b36caefd63a8768be5eb276ee5
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-03-07 23:37:25 +0300

    lesson-5 extra method part-1

---
+++ src/Core/Security/Extensions/ClaimExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Extensions;
+
+public static class ClaimExtensions
+{
+    public static void AddEmail(this ICollection<Claim> claims, string email)
+    {
+        claims.Add(new Claim(ClaimTypes.Email, email));
+    }
+
+    public static void AddName(this ICollection<Claim> claims, string name)
+    {
+        claims.Add(new Claim(ClaimTypes.Name, name));
+    }
+
+    public static void AddNameIdentifier(this ICollection<Claim> claims, string nameIdentifier)
+    {
+        claims.Add(new Claim(ClaimTypes.NameIdentifier, nameIdentifier));
+    }
+
+    public static void AddRoles(this ICollection<Claim> claims, ICollection<string> roles)
+    {
+        foreach (string role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+    }
+
+    //public static void AddRole(this ICollection<Claim> claims, string role)
+    //{
+    //    claims.Add(new Claim(ClaimTypes.Role, role));
+    //}
+}
---
+++ src/Core/Security/JWT/JwtHelper.cs
@@ -0,0 +1,51 @@
+﻿using Core.Security.Encryption;
+using Core.Security.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace Core.Security.JWT;
+public class JwtHelper
+{
+    public IConfiguration Configuration { get;}
+    private readonly TokenOptions _tokenOptions;
+    private DateTime _accessTokenExpiration;
+
+    public JwtHelper(IConfiguration configuration, TokenOptions tokenOptions, DateTime accessTokenExpiration)
+    {
+        Configuration = configuration;
+        _tokenOptions = tokenOptions;
+        _accessTokenExpiration = accessTokenExpiration;
+    }
+
+    public AccessToken CreateToken(User user, IList<OperationClaim> operationClaims)
+    {
+        _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+        SecurityKey securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        SigningCredentials signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+        JwtSecurityToken jwt = CreateJw
+    }
+
+    public RefreshToken CreateRefreshToken(User user, string ipAddress)
+    {
+        RefreshToken refreshToken = new({
+            UserId = user.Id,
+            Token = Random
+        })
+    }
+
+    private IEnumerable<Claim> SetClaims(User user, IList<OperationClaim> operationClaims)
+    {
+        List<Claim> claims = new();
+        return claims;
+    }
+    private string RandomRefreshToken()
+    {
+        byte[] numberByte = new byte[32];
+        using RandomNumberGenerator random = RandomNumberGenerator.Create();
+        random.GetBytes(numberByte);
+        return Convert.ToBase64String(numberByte);
+    }
+}
