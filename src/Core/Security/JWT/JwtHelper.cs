d2d36c3229a121098873b8be3c443b2a231d7d55
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-03-08 23:11:40 +0300

    lesson-5 extra method part-2

--- src/Core/Security/JWT/JwtHelper.cs
+++ src/Core/Security/JWT/JwtHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Security.Encryption;
 using Core.Security.Entities;
+using Core.Security.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -9,7 +10,7 @@ using System.Security.Cryptography;
 namespace Core.Security.JWT;
 public class JwtHelper
 {
-    public IConfiguration Configuration { get;}
+    public IConfiguration Configuration { get; }
     private readonly TokenOptions _tokenOptions;
     private DateTime _accessTokenExpiration;
 
@@ -25,21 +26,54 @@ public class JwtHelper
         _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
         SecurityKey securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
         SigningCredentials signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
-        JwtSecurityToken jwt = CreateJw
+        JwtSecurityToken jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
+        JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+        string? token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+        return new AccessToken { Token = token, Expiration = _accessTokenExpiration };
     }
 
     public RefreshToken CreateRefreshToken(User user, string ipAddress)
     {
-        RefreshToken refreshToken = new({
-            UserId = user.Id,
-            Token = Random
-        })
+        RefreshToken refreshToken =
+            new()
+            { 
+                UserId = user.Id,
+                Token = RandomRefreshToken(),
+                Expires = DateTime.UtcNow.AddDays(7),
+                Created = DateTime.UtcNow,
+                CreatedByIp = ipAddress
+            };
+        return refreshToken;
+    }
+
+    public JwtSecurityToken CreateJwtSecurityToken(
+        TokenOptions tokenOptions,
+        User user,
+        SigningCredentials signingCredentials,
+        IList<OperationClaim> operationClaims)
+    {
+        JwtSecurityToken jwt =
+            new(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims: SetClaims(user, operationClaims),
+                signingCredentials: signingCredentials
+            );
+        return jwt;
     }
 
     private IEnumerable<Claim> SetClaims(User user, IList<OperationClaim> operationClaims)
     {
         List<Claim> claims = new();
+        claims.AddNameIdentifier(user.Id.ToString());
+        claims.AddEmail(user.Email);
+        claims.AddName($"{user.FirstName} {user.LastName}");
+        claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
         return claims;
+
     }
     private string RandomRefreshToken()
     {
