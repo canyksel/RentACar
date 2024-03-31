09fe73544501822c444796f8329079c82b1dda4d
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2024-03-31 23:20:26 +0300

    feat: constant class added into fuel and model

---
+++ src/Application/Features/Fuels/Constants/FuelMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Features.Fuels.Constants;
+
+public class FuelMessages
+{
+    public const string FuelNotExists = "Fuel not exists.";
+    public const string FuelNameExists = "Fuel name exists.";
+}
---
+++ src/Application/Features/Models/Constants/ModelMessages.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Features.Models.Constants;
+
+public class ModelMessages
+{
+    public const string ModelNotExists = "Model not exists.";
+}
